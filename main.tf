09beb767fc638dab96a32c8ef47c03ae3d9c8931
nirmalaram <nirmalaram2008@gmail.com>
2024-05-21 09:02:25 -0700

    adding config files

---
+++ main.tf
@@ -0,0 +1,42 @@
+provider "aws" {
+    region = "us-east-1"
+    
+  
+}
+resource "aws_iam_user" "tuser" {
+    name = "tuser"
+     tags = {
+       name="tfuser"
+     }
+  
+}
+resource "aws_iam_access_key" "keys" {
+    user = aws_iam_user.tuser.name
+
+}
+resource "aws_iam_policy" "tfpolicy" {
+    name = "tfpolicy"
+     policy= <<EOF
+     {
+    "Version": "2012-10-17",
+    "Statement": [
+        {
+        "Action": [
+            "s3:ListAllMyBuckets"
+        ],
+        "Effect": "Allow",
+        "Resource": "*"
+        }]
+     }
+    EOF
+}
+
+resource "aws_iam_user_policy_attachment" "policyattach" {
+    user = aws_iam_user.tuser.name
+    policy_arn = aws_iam_policy.tfpolicy.arn
+  
+}
+
+
+
+  
